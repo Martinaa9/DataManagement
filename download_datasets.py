fd3833917ede056cf13934977eb30464e311423f
Martinaa9 <103061943+Martinaa9@users.noreply.github.com>
2025-02-26 20:04:11 +0100

    dataset cleaning

--- download_datasets.py
+++ download_datasets.py
@@ -11,9 +11,8 @@ def download_datasets():
     datasets = [
         "nehaprabhavalkar/indian-food-101",
         "imtkaggleteam/fast-food-restaurants-across-america",
-        "markmedhat/food-dataset",
         "niharika41298/nutrition-details-for-most-common-foods",
-        # "utkarshsaxenadn/fast-food-nutrition-eda-data-analysis",
+        #"utkarshsaxenadn/fast-food-nutrition-eda-data-analysis",
     ]
 
     # Creare una cartella per i dataset se non esiste
@@ -35,28 +34,58 @@ def load_datasets():
         df_indian_food = pd.read_csv('datasets/indian_food.csv')
         df_fast_food = pd.read_csv('datasets/Datafiniti_Fast_Food_Restaurants.csv')
         df_nutrition = pd.read_csv('datasets/nutrients_csvfile.csv')
+        df_waste_food = pd.read_csv('datasets/Food Waste data and research - by country.csv')
+        df_FastFoodNutrition = pd.read_csv('datasets/FastFoodNutritionMenuV2.csv')
 
         # Selezione di alcune colonne per il dataset indiano
-        df_indian_food = df_indian_food[['name','ingredients','diet','prep_time','cook_time','flavor_profile','course','state','region']]
-        print("Stato (Indian Food):")
-        print(df_indian_food['state'])
+        #df_indian_food = df_indian_food[['name','ingredients','diet','prep_time','cook_time','flavor_profile','course','state','region']]
+        #print("Stato (Indian Food):")
+        #print(df_indian_food['state'])
         
-        return df_indian_food, df_fast_food, df_nutrition
+        return df_indian_food, df_fast_food, df_nutrition, df_waste_food, df_FastFoodNutrition
     
     except Exception as e:
         print(f"Errore nel caricamento dei dataset: {e}")
+
+# Funzione per pulire i dataset eliminando duplicati e valori nulli
+def clean_datasets(datasets):
+    cleaned_datasets = []  # Lista per memorizzare i dataset puliti
+
+    for df in datasets:
+        df_cleaned = df.drop_duplicates().dropna()  # Rimuove duplicati e valori nulli
+        cleaned_datasets.append(df_cleaned)  # Aggiungi il DataFrame pulito alla lista
+    
+    return cleaned_datasets
 
 # Scaricare i dataset
 download_datasets()
 
 # Caricare i dataset
-df_indian_food, df_fast_food, df_nutrition = load_datasets()
+df_indian_food, df_fast_food, df_nutrition, df_waste_food, df_FastFoodNutrition = load_datasets()
 
-# Caricare un ulteriore dataset
-df_food_dataset = pd.read_csv('datasets/food_coded.csv')
-print("*************************")
-print("\nFood Dataset:")
-print(df_food_dataset['GPA'])
+
+#Eliminazione colonne non utili 
+df_indian_food.drop(columns=['cook_time', 'prep_time'], inplace=True)
+df_indian_food.to_csv("datasets/indian_food.csv", index=False)  # Sovrascrive il file originale
+
+df_nutrition.drop(columns=['Sat.Fat', 'Fiber'], inplace=True)
+df_nutrition.to_csv("datasets/nutrients_csvfile.csv", index=False)
+
+df_fast_food.drop(columns=['keys', 'websites', 'dateAdded','dateUpdated', 'address', 'sourceURLs'], inplace=True)
+df_fast_food.to_csv("datasets/FastFoodRestaurants.csv", index=False)
+
+#df_waste_food.drop(columns=['M49 code','Source'],inplace=True)
+#df_waste_food.to_csv('datasets/Food Waste data and research - by country.csv', index=False)
+
+#df_FastFoodNutrition.drop(columns=['Calories from\nFat', 'Saturated Fat\n(g)', 'Trans Fat\n(g)', 'Cholesterol\n(mg)','Sodium \n(mg)','Fiber\n(g)','Sugars\n(g)','Weight Watchers\nPnts'], inplace=True)
+#df_FastFoodNutrition.to_csv("datasets/FastFoodNutritionMenuV2.csv", index=False)
+#Pulire i nomi delle colonne eliminando "\n" e spazi extra e "(g)" dai nomi delle colonne + modifica nome fat 
+df_FastFoodNutrition.columns = df_FastFoodNutrition.columns.str.replace("\n", " ") \
+    .str.replace("(g)", "", regex=False) \
+    .str.replace("Total Fat", "Fat") \
+    .str.strip()
+df_FastFoodNutrition.to_csv("datasets/FastFoodNutritionMenuV2.csv", index=False)
+
 
 # Funzione per scrivere in un file i nomi delle colonne di ogni dataset
 def write_columns_to_file(filename):
@@ -70,8 +99,11 @@ def write_columns_to_file(filename):
         f.write("\nNutrition Dataset columns:\n")
         for col in df_nutrition.columns:
             f.write(f"- {col}\n")
-        f.write("\nFood Dataset columns:\n")
-        for col in df_food_dataset.columns:
+        f.write("\nFast food nutrition columns:\n")
+        for col in df_FastFoodNutrition.columns:
+            f.write(f"- {col}\n")
+        f.write("\nFood waste columns:\n")
+        for col in df_waste_food.columns:
             f.write(f"- {col}\n")
     print(f"Elenco delle colonne salvato in '{filename}'.")
 
